82d751e7592c4227ad235a8934b4efccdacb8313
Ronnie McGrog <ronnie@box.now.im>
2020-08-18 17:19:01 +0300

    выпиливание ключей из остальных скрытых дирректорий

--- idea/datagrip-Trial-Reset.bat
+++ idea/datagrip-Trial-Reset.bat
@@ -2,6 +2,12 @@ rem https://stackoverflow.com/a/57236125
 
 cd "C:%HOMEPATH%\.DataGrip*\config"
 rmdir "eval" /s /q
-del "options\other.xml"
+
+cd "%LocalAppData%\JetBrains\DataGrip*"
+rmdir "eval" /s /q
+
+cd "%AppData%\JetBrains\DataGrip*"
+rmdir "eval" /s /q
+
 reg delete "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\jetbrains\datagrip" /f
 rem reg delete "HKEY_CURRENT_USER\Software\JavaSoft\Prefs" /v "/Jet/Brains./User/Id/On/Machine" /f
